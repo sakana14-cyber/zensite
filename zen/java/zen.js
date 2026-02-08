2034a0b0da3a878dd6a524612c9e474246d11767
sakana14-cyber <kyomon39@gmail.com>
2026-02-09 00:08:03 +0900

    Delete zen directory

--- zen/java/zen.js
+++
@@ -1,6 +0,0 @@
-// フォーム送信時のメッセージ表示
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // ページリロードを防ぐ
-    document.getElementById("contactForm").style.display = "none";
-    document.getElementById("thanksMessage").style.display = "block";
-});
